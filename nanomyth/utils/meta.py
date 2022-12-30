5c263d277817f29442727a9f4419c8430673f570
Dixie Flatline <umi0451@gmail.com>
2022-12-30 14:19:50 +0200

    Delegated delegate was trying to get the most original name of the called function instead of the immediate delegated delegate.

--- nanomyth/utils/meta.py
+++ nanomyth/utils/meta.py
@@ -1,13 +1,13 @@
 import inspect
 
-def wrap_docstring(f):
+def wrap_docstring(f, original_delegate=None):
 	""" Fixes docstring of wrapped object to propagate the original docstring
 	and appends reference to the original method.
 	"""
 	def wrapper(*args, **kwargs): # pragma: no cover -- dummy definition, should not be called at all.
 		return f(*args, **kwargs)
 	wrapper.__doc__ = (inspect.getdoc(f) or "") + "\n\nNote: See {0} for details.".format(f.__qualname__)
-	wrapper.__wrapped_delegate__  = f
+	wrapper.__wrapped_delegate__  = original_delegate
 	return wrapper
 
 class Delegate:
@@ -18,12 +18,21 @@ class Delegate:
 		"""
 		self.member = member
 		self.base_method = base_method
-		while hasattr(self.base_method, '__wrapped_delegate__'):
+		if hasattr(self.base_method, '__wrapped_delegate__'):
 			self.base_method = self.base_method.__wrapped_delegate__
-		self.method = self.base_method.__name__
+			self.method = None
+		else:
+			self.method = self.base_method.__name__
 	def __get__(self, obj, objtype):
 		if obj is None:
 			# When object does not exist, it probably means that inspect tries to get docstring for the class member.
 			# In this case return original method with slightly adjusted docstring to indicate delegation.
-			return wrap_docstring(self.base_method)
+			return wrap_docstring(self.base_method, original_delegate=self)
+		if self.method is None:
+			# Means that delegate was created to already delegated function,
+			# and we need to get delegated's delegate name by questioning member's dict for the original delegate.
+			for method, v in getattr(obj, self.member).__class__.__dict__.items():
+				if v is self.base_method:
+					self.method = method
+					return getattr(getattr(obj, self.member), method)
 		return getattr(getattr(obj, self.member), self.method)
--- nanomyth/utils/test/test_meta.py
+++ nanomyth/utils/test/test_meta.py
@@ -11,26 +11,26 @@ class MockOriginalClass:
 		return 'bar called: {0}'.format(self.value)
 
 class Delegator:
-	foo = meta.Delegate('member', MockOriginalClass.foo)
+	delegate_foo = meta.Delegate('member', MockOriginalClass.foo)
 	bar = meta.Delegate('member', MockOriginalClass.bar)
 	def __init__(self):
 		self.member = MockOriginalClass()
 		self.member.value = 'baz'
 
 class SuperDelegator:
-	foo = meta.Delegate('member', Delegator.foo)
+	super_foo = meta.Delegate('submember', Delegator.delegate_foo)
 	def __init__(self):
-		self.member = Delegator()
+		self.submember = Delegator()
 
 class TestDelegatedMethods(unittest.TestCase):
 	def should_adjust_docstsring_for_delegated_methods(self):
-		self.assertEqual(inspect.getdoc(Delegator.foo), 'Description of foo. \n\nNote: See MockOriginalClass.foo for details.')
+		self.assertEqual(inspect.getdoc(Delegator.delegate_foo), 'Description of foo. \n\nNote: See MockOriginalClass.foo for details.')
 		self.assertEqual(inspect.getdoc(Delegator.bar), "Bar's description. \n\nNote: See MockOriginalClass.bar for details.")
 	def should_call_delegated_method(self):
 		delegator = Delegator()
-		self.assertEqual(delegator.foo(), 'foo called')
-		self.assertEqual(delegator.foo(), 'foo called')
+		self.assertEqual(delegator.delegate_foo(), 'foo called')
+		self.assertEqual(delegator.delegate_foo(), 'foo called')
 		self.assertEqual(delegator.bar(), 'bar called: baz')
 	def should_delegate_method_through(self):
 		delegator = SuperDelegator()
-		self.assertEqual(delegator.foo(), 'foo called')
+		self.assertEqual(delegator.super_foo(), 'foo called')
